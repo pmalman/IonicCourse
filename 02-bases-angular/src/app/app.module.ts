7a833e7fba557d98c2daeec34de84a67630da334
pmalman <pmaldonadomn@gmail.com>
2021-10-15 13:00:59 +0200

    2 Bases de angular

---
+++ 02-bases-angular/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+import {HttpClientModule} from '@angular/common/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    // AboutComponent,
+    // ContactComponent,
+    // HomeComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
